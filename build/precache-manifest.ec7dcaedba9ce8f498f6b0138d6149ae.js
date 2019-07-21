d5c91c7459b0ccc2c78451be3edabe0ccbb17aae
AlexPrime <pochtarev.dev@gmail.com>
2019-07-21 23:51:16 +0300

    add build "release"

---
+++ build/precache-manifest.ec7dcaedba9ce8f498f6b0138d6149ae.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8bfa32c3de9bd6a16f6804176ad6ca82",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a14947a04fe300d3ed7",
+    "url": "/static/css/main.88b7b8e4.chunk.css"
+  },
+  {
+    "revision": "06f44d7c6eeee966ea35",
+    "url": "/static/js/2.34ad89f7.chunk.js"
+  },
+  {
+    "revision": "9a14947a04fe300d3ed7",
+    "url": "/static/js/main.f3205d48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
