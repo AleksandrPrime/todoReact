7483d13c9eb71690a28fd5f887bc138235231d18
AlexPrime <pochtarev.dev@gmail.com>
2019-07-30 00:47:14 +0300

    add build gh-pages deploy fix

---
+++ build/precache-manifest.2fa0c781af86389031c4df926a38c823.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cca057067d3aca33e9aa06ee9053ebf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9a14947a04fe300d3ed7",
+    "url": "/static/css/main.88b7b8e4.chunk.css"
+  },
+  {
+    "revision": "81aabbabf19fd771eebc",
+    "url": "/static/js/2.93365f9e.chunk.js"
+  },
+  {
+    "revision": "9a14947a04fe300d3ed7",
+    "url": "/static/js/main.f3205d48.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
